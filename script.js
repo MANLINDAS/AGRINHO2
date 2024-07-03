f144be47c5e5fb5e77d9d4d54af08af5bffe7e7d
MANLINDAS <159142596+MANLINDAS@users.noreply.github.com>
2024-07-03 08:30:15 -0300

    .

---
+++ script.js
@@ -0,0 +1,7 @@
+function saibaMais() {
+    alert('Mais informações sobre a importância do campo para a cidade.');
+}
+
+function explore() {
+    alert('Explorando mais conteúdo relacionado ao campo e à cidade.');
+}
