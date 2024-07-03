9fc0f52010f6d7003970f87985b0c9ee12802cf3
MANLINDAS <159142596+MANLINDAS@users.noreply.github.com>
2024-07-03 16:59:56 -0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// Adicione qualquer código JavaScript interativo aqui (opcional)
+
 document.addEventListener('DOMContentLoaded', function () {
     console.log('Página carregada.');
-    // Adicione qualquer funcionalidade interativa, se necessário
 });
 
